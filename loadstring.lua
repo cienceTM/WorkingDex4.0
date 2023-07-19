887ea3bc77d86eba1af20a9feb1c3455d59a0611
cienceTM <83795793+cienceTM@users.noreply.github.com>
2023-07-19 18:38:27 -0500

    Create loadstring.lua

---
+++ loadstring.lua
@@ -0,0 +1,4 @@
+getfenv(1).currentTheme = "Graphite" -- Change to an valid theme(example: "White")
+-- Themes: Graphite, Platinum, White, Red, Cyan, Yellow, Orange
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/cienceTM/WorkingDex4.0/main/Dex4main.lua", true))()
